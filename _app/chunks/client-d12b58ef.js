f45350e47d349f2052e3536d2b5442d25b75311c
pgtk-web <pgtk.web@gmail.com>
2021-05-25 11:36:54 +0600

    q

--- _app/chunks/client-d12b58ef.js
+++
@@ -1,1 +0,0 @@
-import{V as i}from"./vendor-cf24f899.js";const t=i.client("https://gtk-prismic-sveltekit-new.cdn.prismic.io/api/v2");export{t as C};
--- _app/chunks/index.svelte_svelte&type=style&lang-937b2acc.js
+++
@@ -1,1 +0,0 @@
-import{O as i}from"./vendor-63f5ebb1.js";const t=i.client("https://gtk-prismic-sveltekit-new.cdn.prismic.io/api/v2");export{t as C};
